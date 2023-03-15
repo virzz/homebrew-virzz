bd198f20133c3e5ab491c7cf71ad1b7bf77a9b53
陌竹 <mozhu233@outlook.com>
2023-03-15 23:15:41 +0800

    ✨ Feat(*): Update

--- Formula/god.rb
+++ Formula/god.rb
@@ -1,21 +1,17 @@
 class God < Formula
   desc "Cyber swiss army knife for terminal"
   homepage "https://github.com/virzz/virzz"
-  url "https://github.com/virzz/virzz/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "622bc4230a66e0c540cbac69947839e43fba587393941cc04f9820c7e252cf1e"
+  url "https://github.com/virzz/virzz.git",
+      tag:      "v0.1.7",
+      revision: "f3d6c0f01d04b19825174446fc5b97b65f0aa9f7"
   license "MIT"
   head "https://github.com/virzz/virzz.git", branch: "master"
+  license "MIT"
 
-  bottle do
-    root_url "https://ghcr.io/v2/virzz/virzz"
-    sha256 cellar: :any_skip_relocation, monterey:     "7c28d5da4f78ba5a785cc06016115d4246cf0922dcc0f03a44c5b454526f41e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e2e6cd08358a20b2ebbf94d273a31ab2d049ff857826d1712f92cec13525281d"
-  end
-
-  depends_on "go"
+  depends_on "go" => :build
 
   def install
-    system "go", "run", "./internal/_compile", "-R", "-V", "v0.1.6", "god"
+    system "go", "run", "./internal/_compile", "-R", "-V", version.to_s, "-G", Utils.git_head.to_s, "god"
     bin.install "build/god" => "god"
   end
 
