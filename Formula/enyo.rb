f765130dfc92b371dcaa2253695b94258fc7ad4c
陌竹 <mozhu233@outlook.com>
2023-03-28 01:51:03 +0800

    enyo: add 0.1.8 bottle

--- Formula/enyo.rb
+++ Formula/enyo.rb
@@ -7,6 +7,13 @@ class Enyo < Formula
   license "MIT"
   head "https://github.com/virzz/virzz.git", branch: "master"
 
+  bottle do
+    root_url "http://virzz-img.ctfhub.com/tools/virzz"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bf0587f61326c193925a8759d4296314a463d8c699d0dd04d55e60c16279d35f"
+    sha256 cellar: :any_skip_relocation, monterey:      "2382803df9d058c3b865870b7305dd4e80a98336dd1a30bd9636d0a60dcc34f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a49e4ccf8bb658019f8ccf80a85cfa306541f4ae4544f6aa7823c5979cc9c155"
+  end
+
   depends_on "go" => :build
 
   def install
