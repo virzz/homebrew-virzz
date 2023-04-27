be0fbeea823e7c8c7c89ff64e2561dfcc9efde7b
陌竹 <mozhu233@outlook.com>
2023-04-27 19:16:17 +0800

    ✨ Feat(*): Update Enyo v0.1.9 (#8)
    * ✨ Feat(*): Update Enyo v0.1.9
    
    * enyo: update 0.1.9 bottle.

--- Formula/enyo.rb
+++ Formula/enyo.rb
@@ -2,16 +2,16 @@ class Enyo < Formula
   desc "Cyber swiss army knife for terminal"
   homepage "https://github.com/virzz/virzz"
   url "https://github.com/virzz/virzz.git",
-      tag:      "v0.1.8",
-      revision: "372ec633730c75a0f51c9fb6fb914505032b6ad6"
+      tag:      "v0.1.9",
+      revision: "c64de5bf7e7d235f5499271544b08ab48bcfa40b"
   license "MIT"
   head "https://github.com/virzz/virzz.git", branch: "master"
 
   bottle do
     root_url "http://virzz-img.ctfhub.com/tools/virzz"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bf0587f61326c193925a8759d4296314a463d8c699d0dd04d55e60c16279d35f"
-    sha256 cellar: :any_skip_relocation, monterey:      "2382803df9d058c3b865870b7305dd4e80a98336dd1a30bd9636d0a60dcc34f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a49e4ccf8bb658019f8ccf80a85cfa306541f4ae4544f6aa7823c5979cc9c155"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "aaee781cc2d678beae6f4970656ecc59f5b7d162c6f56a73e4fa28ba0fddbbee"
+    sha256 cellar: :any_skip_relocation, monterey:     "ac515321876a8de871680bf3b1face34789d1d50ce54dd382dd6a992c48d3103"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5ecfc64a3fdd05b5e3a30f20a6126151f21c6140cf04823a01f429bf0c4aee97"
   end
 
   depends_on "go" => :build
