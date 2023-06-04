525646c6884e7b93d7f89aaadc92b3f91d3b6ac3
陌竹 <mozhu233@outlook.com>
2023-06-04 10:48:30 +0800

    ✨ Feat(*): Update Enyo v0.1.10

--- Formula/enyo.rb
+++ Formula/enyo.rb
@@ -2,17 +2,10 @@ class Enyo < Formula
   desc "Cyber swiss army knife for terminal"
   homepage "https://github.com/virzz/virzz"
   url "https://github.com/virzz/virzz.git",
-      tag:      "v0.1.9",
-      revision: "c64de5bf7e7d235f5499271544b08ab48bcfa40b"
+      tag:      "v0.1.10",
+      revision: "48882e703a9ab20268b613d15973a0ca67937b71"
   license "MIT"
   head "https://github.com/virzz/virzz.git", branch: "master"
-
-  bottle do
-    root_url "http://virzz-img.ctfhub.com/tools/virzz"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "aaee781cc2d678beae6f4970656ecc59f5b7d162c6f56a73e4fa28ba0fddbbee"
-    sha256 cellar: :any_skip_relocation, monterey:     "ac515321876a8de871680bf3b1face34789d1d50ce54dd382dd6a992c48d3103"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5ecfc64a3fdd05b5e3a30f20a6126151f21c6140cf04823a01f429bf0c4aee97"
-  end
 
   depends_on "go" => :build
 
